3055bbfa0b4c9d67d5c4a79cfe9e7c1ca09eff08
cchhumnetlify <charlotte.chhum@netlify.com>
2024-05-20 18:31:26 +0200

    api call

---
+++ api-call/apicall.js
@@ -0,0 +1,36 @@
+// fetch("https://pokeapi.co/api/v2/pokemon/10")
+//   .then(response => {
+//     if(!response.ok) {
+//         throw new Error("Pokemon not found");
+//     }
+//     return response.json();
+//   })
+//   .then(data => console.log(data.name))
+//   .catch(error => console.error(error));
+
+
+fetchPokemon();
+
+async function fetchPokemon(){
+    try {
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok){
+            throw new Error("Could not find pokemon");
+        }
+
+        const data = await response.json();
+
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error){
+        console.error(error);
+    }
+}
--- netlify/functions/pokeapi.mjs
+++ netlify/functions/pokeapi.mjs
@@ -1,3 +1,7 @@
-export default async (req, context) => {
-    return new Response("Hello, world!");
-  };
+// export default async (req, context) => {
+//     return new Response("Hello, world!");
+//   };
+
+fetch("https://pokeapi.co/api/v2/pokemon/7")
+  .then(response => console.log(response))
+  .catch(error => console.error(error))
